59a2f84eeb312870ec34e06cd4cc71cd1467672e
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2022-02-18 00:42:38 +0200

    configure db, credentials, circleci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -32,8 +35,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 Dir['spec/support/sections/*.rb', 'spec/support/**/**/*.rb'].each { |file| require Rails.root.join(file) }
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+
 RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
