4feb92b29ef3a5e330c075372b4487e7336b691d
Valeriy.Pogrebniak <km270985pvg@bitbucket.org>
2022-02-21 13:16:51 +0200

    add page object description for header, footer, home page

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Category do
+  subject(:category) { described_class.new }
+
+  it 'is valid with provided title' do
+    category.title = FFaker::Book.unique.genre
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    category.title = nil
+    expect(category).not_to be_valid
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,10 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter ['spec', 'app/jobs', 'app/mailers', 'app/models']
+  minimum_coverage 95
+end
 
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
