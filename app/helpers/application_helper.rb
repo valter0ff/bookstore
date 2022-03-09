498ecd39508ce53303d7bb2ff5af3533dd639aa6
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-03-09 16:17:50 +0200

    update application helper to add some base presenter methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def present(model)
+    klass = "#{model.class}Presenter".constantize
+    presenter = klass.new(model, self)
+    yield(presenter) if block_given?
+  end
+
+  def view_object(name, args = {})
+    class_name = if name.is_a?(Symbol)
+                   name.to_s.titleize.split.join
+                 else
+                   name.split('/').map { |n| n.titleize.sub(' ', '') }.join('::')
+                 end
+    class_name.constantize.new(self, args)
+  end
 end
