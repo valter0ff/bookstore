277ab7dbbcc3ab08bf1a752ae345c1411358e743
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-03-09 17:02:13 +0200

    add pagination

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,10 @@ class BooksController < ApplicationController
     @category = CategorySelector.call(@categories, self)
     @books = BooksSelector.call(@category, self)
     @catalog = CatalogViewObject.new(view_context)
-    render 'catalog'
+    @pagy, @books = pagy_array(@books.to_a, items: 12, link_extra: 'data-remote="true"')
+    respond_to do |format|
+      format.html { render 'catalog' }
+      format.js
+    end
   end
 end
