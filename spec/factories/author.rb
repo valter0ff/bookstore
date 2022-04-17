0a434db13694d9958abd2e46ee424830ade9fb64
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-04-17 15:15:28 +0300

    add Draper gem to app

--- spec/factories/author.rb
+++ spec/factories/author.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :author do
     first_name { FFaker::Name.unique.first_name }
     last_name  { FFaker::Name.unique.last_name }
+    description { FFaker::Lorem.paragraph }
   end
 end
--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'capybara'
+require 'capybara/apparition'
 require 'capybara/cuprite'
 require 'capybara-screenshot/rspec'
 require 'capybara/rspec'
@@ -10,7 +11,27 @@ require 'site_prism/all_there'
 
 # Capybara.current_driver = :selenium_chrome
 Capybara.asset_host = 'http://localhost:3000'
+# Capybara.javascript_driver = :chrome_headless
 Capybara.javascript_driver = :cuprite
+# Capybara.javascript_driver = :apparition
+
 Capybara.register_driver(:cuprite) do |app|
   Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], inspector: true)
 end
+
+Capybara.register_driver :apparition do |app|
+  Capybara::Apparition::Driver.new(app, headless: false)
+end
+# Capybara.register_driver :chrome_headless do |app|
+# #   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+# #     chromeOptions: {
+# #       args: %w[ no-sandbox headless disable-gpu ]
+# #     }
+# #   )
+#   options = Selenium::WebDriver::Chrome::Options.new
+#   options.add_argument('--no-sandbox')
+# #   options.add_argument('--disable-gpu')
+#   options.add_argument('--disable-popup-blocking')
+#
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+# end
