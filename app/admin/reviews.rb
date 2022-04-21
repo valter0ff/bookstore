6624f374db31a75b72a98f85a8c17db3cacf4d31
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-04-21 12:50:10 +0300

    add 'activeadmin_addons' gem

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -6,44 +6,42 @@ ActiveAdmin.register Review do
   permit_params :title, :body, :status, :book_id, :user_account_id
 
   includes :book, :user_account
-  
-  actions :all, except: [:edit, :new, :create]
-  
-  scope :unprocessed
-  scope :approved
-  scope :rejected
-  
-  index do 
+
+  actions :all, except: %i[edit new create]
+
+  scope I18n.t('reviews.admin.new'), :unprocessed
+  scope :approved, group: :processed
+  scope :rejected, group: :processed
+
+  index do
     selectable_column
     column :book
     column :title
-    column 'Date', :created_at
+    column I18n.t('reviews.admin.date'), :created_at
     column :user_account
-    column :status
+    tag_column :status
     actions
   end
 
   member_action :approve, method: :put do
     resource.approved!
-    redirect_to admin_reviews_path, notice: "Approved!"
-  end
-  
-  member_action :reject, method: :put do
-    resource.rejected!
-    redirect_to admin_reviews_path, notice: "Rejected!"
-  end
-  
-  action_item :approve, only: :show do
-     link_to 'Approve', approve_admin_review_path(review), method: :put
-  end
-  
-  action_item :reject, only: :show do
-    link_to 'Reject', reject_admin_review_path(review), method: :put
-  end
-  
-  show do
-    default_main_content 
+    redirect_to admin_reviews_path, notice: I18n.t('reviews.admin.approved')
   end
 
+  member_action :reject, method: :put do
+    resource.rejected!
+    redirect_to admin_reviews_path, notice: I18n.t('reviews.admin.rejected')
+  end
+
+  action_item :approve, only: :show do
+    link_to I18n.t('reviews.admin.approve'), approve_admin_review_path(review), method: :put
+  end
+
+  action_item :reject, only: :show do
+    link_to I18n.t('reviews.admin.reject'), reject_admin_review_path(review), method: :put
+  end
+
+  show do
+    default_main_content
+  end
 end
- 
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,13 +5,13 @@ class Review < ApplicationRecord
 
   belongs_to :book
   belongs_to :user_account
-  
+
   enum status: STATUSES
-  
-  validates :title, presence: true, 
+
+  validates :title, presence: true,
                     length: { maximum: Constants::Review::TITLE_MAX_SIZE },
                     format: { with: Constants::Review::COMMON_REGEXP }
-  validates :body, presence: true, 
+  validates :body, presence: true,
                    length: { maximum: Constants::Review::BODY_MAX_SIZE },
                    format: { with: Constants::Review::COMMON_REGEXP }
 end
