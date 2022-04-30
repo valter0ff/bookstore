448c163d6a98e7e5d124a1a2b8cc826306103ef1
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-04-30 12:36:11 +0300

    add batch actions approve and reject

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Review do
-  config.filters = false
-
   permit_params :title, :body, :status, :book_id, :user_account_id
 
   includes :book, :user_account
@@ -17,11 +15,15 @@ ActiveAdmin.register Review do
     selectable_column
     column :book
     column :title
+    column :rating
     column I18n.t('reviews.admin.date'), :created_at
     column :user_account
     tag_column :status
     actions
   end
+
+  preserve_default_filters!
+  filter :book_id, label: I18n.t('reviews.admin.book_id')
 
   member_action :approve, method: :put do
     resource.approved!
@@ -44,6 +46,22 @@ ActiveAdmin.register Review do
   batch_action :destroy, confirm: I18n.t('active_admin.delete_confirmation') do |ids|
     Review.where(id: ids).destroy_all
     redirect_to admin_reviews_path, notice: I18n.t('active_admin.batch_actions.succesfully_destroyed',
+                                                   count: ids.count,
+                                                   model: resource_class.to_s.downcase,
+                                                   plural_model: resource_class.to_s.pluralize.titleize.downcase)
+  end
+
+  batch_action :approve do |ids|
+    Review.where(id: ids).each(&:approved!)
+    redirect_to admin_reviews_path, notice: I18n.t('active_admin.batch_actions.succesfully_updated',
+                                                   count: ids.count,
+                                                   model: resource_class.to_s.downcase,
+                                                   plural_model: resource_class.to_s.pluralize.titleize.downcase)
+  end
+
+  batch_action :reject do |ids|
+    Review.where(id: ids).each(&:rejected!)
+    redirect_to admin_reviews_path, notice: I18n.t('active_admin.batch_actions.succesfully_updated',
                                                    count: ids.count,
                                                    model: resource_class.to_s.downcase,
                                                    plural_model: resource_class.to_s.pluralize.titleize.downcase)
