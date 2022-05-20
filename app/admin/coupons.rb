3b87467a6f6afd7dd621f58030e392215469a535
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-05-20 13:05:42 +0300

    drop useless code from admin coupons

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -12,28 +12,7 @@ ActiveAdmin.register Coupon do
     column :code
     column :discount
     tag_column :status
-    actions defaults: true do |coupon|
-      if coupon.active?
-        link_to I18n.t('coupons.admin.deactivate'), deactivate_admin_coupon_path(coupon), method: :put
-      end
-    end
-  end
-
-  member_action :deactivate, method: :put do
-    resource.used!
-    redirect_to admin_coupons_path, notice: I18n.t('flash.actions.update.notice', resource_name: resource_class.to_s)
-  end
-
-  action_item :deactivate, only: :show do
-    link_to I18n.t('coupons.admin.deactivate'), deactivate_admin_coupon_path(coupon), method: :put
-  end
-
-  batch_action :deactivate do |ids|
-    Coupon.where(id: ids).each(&:used!)
-    redirect_to admin_coupons_path, notice: I18n.t('active_admin.batch_actions.succesfully_updated',
-                                                   count: ids.count,
-                                                   model: resource_class.to_s.downcase,
-                                                   plural_model: resource_class.to_s.pluralize.titleize.downcase)
+    actions
   end
 
   show do
