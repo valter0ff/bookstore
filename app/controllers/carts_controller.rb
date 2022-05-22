e958294383f1d4b4347ec7de34d8fbea9f403bda
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-05-22 16:01:54 +0300

    add carts controller with views, partials, locales

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class CartsController < ClientController
+  before_action :find_coupon, only: :update
+
+  def show
+    @cart_items = @order.cart_items.includes(book: :pictures)
+  end
+
+  def update
+    if @coupon.try(:active?)
+      @order.update(coupon: @coupon)
+      redirect_to cart_path, notice: I18n.t('carts.show.coupon_applied')
+    else
+      redirect_to cart_path, alert: I18n.t('carts.show.coupon_rejected')
+    end
+  end
+
+  private
+
+  def cart_params
+    params.require(:order).permit(coupon: :code)
+  end
+
+  def find_coupon
+    @coupon = Coupon.find_by(code: cart_params[:coupon][:code])
+  end
+end
