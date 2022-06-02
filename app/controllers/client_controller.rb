a6a0dc9bb8614298a20b2a8029b339d68d44c089
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-02 18:09:50 +0300

    update Clients controller, application helper and cart view

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -6,6 +6,8 @@ class ClientController < ApplicationController
   before_action :set_all_categories
   before_action :set_order
   before_action :set_total_books_count
+  before_action :store_user_location!, if: :storable_location?
+
 
   private
 
@@ -19,5 +21,17 @@ class ClientController < ApplicationController
 
   def set_total_books_count
     @total_books_count = @order.cart_items.sum(&:books_count)
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def after_sign_in_path_for(_resource)
+    stored_location_for(_resource) || super
   end
 end
