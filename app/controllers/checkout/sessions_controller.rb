bf90bd8d943ba899b0492af975557f69f35a1eee
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-14 01:12:53 +0300

    some changes in checkout session controller

--- app/controllers/checkout/sessions_controller.rb
+++ app/controllers/checkout/sessions_controller.rb
@@ -3,7 +3,7 @@
 module Checkout
   class SessionsController < Devise::SessionsController
     def sign_up
-      @user = UserAccount.new(email: params[:user_account][:email], password: generate_password)
+      @user = UserAccount.new(email: permitted_params[:email], password: generate_password)
       if @user.save
         @user.send_reset_password_instructions
         sign_in(@user)
@@ -14,6 +14,10 @@ module Checkout
     end
 
     private
+
+    def permitted_params
+      params.require(:user_account).permit(:email)
+    end
 
     def generate_password
       Devise.friendly_token.first(8) + rand(10).to_s
