ed68f752e10b7b57103317ab6fffe846ddb288b8
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-15 01:18:45 +0300

    update a lot of views

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -4,6 +4,28 @@ class OrderDecorator < ApplicationDecorator
   delegate_all
   decorates_association :cart_items
 
+  def subtotal_price
+    price_with_currency(subtotal_value)
+  end
+
+  def discount
+    price_with_currency(discount_value)
+  end
+
+  def total_price
+    price_with_currency(total_value)
+  end
+
+  def shipping_amount
+    price_with_currency(shipping_method.price || 0)
+  end
+
+  def total_with_shipping
+    price_with_currency(total_with_shipping_value)
+  end
+
+  private
+
   def subtotal_value
     cart_items.sum(&:subtotal_value)
   end
@@ -18,15 +40,10 @@ class OrderDecorator < ApplicationDecorator
     subtotal_value - discount_value.to_i
   end
 
-  def subtotal_price
-    price_with_currency(subtotal_value)
-  end
+  def total_with_shipping_value
+    total = subtotal_value + shipping_method.price
+    return 0 if total <= discount_value.to_i
 
-  def discount
-    price_with_currency(discount_value)
-  end
-
-  def total_price
-    price_with_currency(total_value)
+    total - discount_value.to_i
   end
 end
