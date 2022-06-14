6b8f0bfba7faf5031a9a3bd131092a4db16defff
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-15 01:26:33 +0300

    add deliveries and  payments controllers

--- app/controllers/checkout/deliveries_controller.rb
+++ app/controllers/checkout/deliveries_controller.rb
@@ -2,6 +2,29 @@
 
 module Checkout
   class DeliveriesController < BaseCheckoutController
+    before_action :set_shipping_methods
+
     def new; end
+
+    def update
+      shipping_method = ShippingMethod.find_by(id: params[:order][:shipping_method_id])
+      if shipping_method.present?
+        @order.update(permitted_params)
+        redirect_to new_checkout_payment_path, notice: I18n.t('checkout.deliveries.new.shipping_method_saved')
+      else
+        flash.now[:alert] = I18n.t('checkout.deliveries.new.choose_method')
+        render :new
+      end
+    end
+
+    private
+
+    def permitted_params
+      params.require(:order).permit(:shipping_method_id)
+    end
+
+    def set_shipping_methods
+      @shipping_methods = ShippingMethod.all.decorate
+    end
   end
 end
---
+++ app/controllers/checkout/payments_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Checkout
+  class PaymentsController < BaseCheckoutController
+    def new; end
+  end
+end
