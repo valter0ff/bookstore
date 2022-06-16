30d7069ecd6fa73b59782cd863784a030f03789b
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-17 00:20:27 +0300

    fix checkout delivery js

--- app/javascript/packs/checkout/checkout_order_summary.js
+++ app/javascript/packs/checkout/checkout_order_summary.js
@@ -1,7 +1,7 @@
 function checkoutOrderSummary() {
   let currency, discount, order_subtotal, order_total, shipping, shipping_value;
   currency = $('.order-subtotal').text().match(/\D/)[0];
-  shipping = $(":radio:checked").parents('tr').find('.price').text();
+  shipping = $(":radio:checked").parents('tr').find('.shipping-price').text();
   discount = parseFloat($('.discount').text().match(/[\d.]+/g));
   order_subtotal = parseFloat($('.order-subtotal').text().match(/[\d.]+/g));
   shipping_value = parseFloat(shipping.match(/[\d.]+/g));
@@ -9,7 +9,7 @@ function checkoutOrderSummary() {
   subtotal_w_shipping = order_subtotal + shipping_value
   order_total = (subtotal_w_shipping <= discount) ? 0 : (subtotal_w_shipping - discount)
 
-  $('.shipping').text(shipping);
+  $('.shipping-amount').text(shipping);
   $('.order-total').text(currency + order_total.toFixed(2));
 };
 
