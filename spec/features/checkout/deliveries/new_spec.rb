415b098861f60d3c122c5aa6406eed2715a5962b
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-16 16:12:00 +0300

    small fix

--- spec/features/checkout/deliveries/new_spec.rb
+++ spec/features/checkout/deliveries/new_spec.rb
@@ -29,15 +29,22 @@ RSpec.describe 'Checkout::Deliveries->New', type: :feature do
       it_behaves_like 'checkout progressbar have all elements'
     end
 
-
-    context 'when shipping form' do
+    context 'when shipping form and shipping method info block' do
       let(:shipping_form) { deliveries_new_page.shipping_methods_form }
+      let(:shipping_method_block) { shipping_form.shipping_method_info.first }
 
-      it 'all elements present' do
+      it 'form elements present' do
         expect(shipping_form).to have_shipping_name_title(text: I18n.t('checkout.deliveries.new.method'))
         expect(shipping_form).to have_shipping_time_title(text: I18n.t('checkout.deliveries.new.days'))
         expect(shipping_form).to have_shipping_price_title(text: I18n.t('checkout.deliveries.new.price'))
         expect(shipping_form).to have_shipping_method_info
+      end
+
+      it 'info block elements present' do
+        expect(shipping_method_block).to have_shipping_name
+        expect(shipping_method_block).to have_shipping_time
+        expect(shipping_method_block).to have_shipping_price
+        expect(shipping_method_block).to have_radio_button
       end
     end
 
