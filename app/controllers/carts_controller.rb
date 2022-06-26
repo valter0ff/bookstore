8998e1e48b2f4293afcd66b5824b37fc6ab8336f
Valerii Pohrebniak <km270985pvg@gmail.com>
2022-06-26 15:58:27 +0300

    change carts show logic

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,11 @@
 
 class CartsController < ClientController
   def show
-    @order = @order.decorate
-    @cart_items = @order.cart_items.includes(book: :pictures)
+    if @total_books_count.zero?
+      flash.now[:alert] = I18n.t('carts.show.cart_empty')
+    else
+      @order = @order.decorate
+      @cart_items = @order.cart_items.includes(book: :pictures)
+    end
   end
 end
